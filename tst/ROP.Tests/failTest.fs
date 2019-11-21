02997aaba6bb07f81dec26e0e1cc2ff1f3c4c760
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2019-11-21 19:12:37 +0100

    Initial hookup

---
+++ tst/ROP.Tests/failTest.fs
@@ -0,0 +1,12 @@
+﻿module failTest
+
+open Xunit
+open SFX.ROP
+open FsCheck.Xunit
+
+[<Property>]
+[<Trait("Category", "Unit")>]
+let ``fail works`` (x: int) =
+  match fail x with
+  | Failure _ -> true
+  | _ -> false
