5f3358fe48e879c4f3b6a7135a9defd23c084713
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2019-12-17 01:27:34 +0100

    Fixed stupidities

--- src/ROP/Library.fs
+++ src/ROP/Library.fs
@@ -1,67 +1,70 @@
-﻿module SFX.ROP
+﻿namespace SFX.ROP
 
-open SFX.ROP.Types
+[<AutoOpen>]
+module Impl =
 
-// convert a single value into a two-track result
-let succeed = Success
+    open SFX.ROP
 
-// convert a single value into a two-track result
-let fail = Failure
+    // convert a single value into a two-track result
+    let succeed = Success
 
-// apply either a success function or failure function
-let either successFunc failureFunc twoTrackInput =
-    match twoTrackInput with
-    | Success s -> successFunc s
-    | Failure f -> failureFunc f
+    // convert a single value into a two-track result
+    let fail = Failure
 
-// convert a switch function into a two-track function
-let bind f = 
-    fail |> either f
+    // apply either a success function or failure function
+    let either successFunc failureFunc twoTrackInput =
+        match twoTrackInput with
+        | Success s -> successFunc s
+        | Failure f -> failureFunc f
 
-// pipe a two-track value into a switch function 
-let (>>=) x f = 
-    bind f x
+    // convert a switch function into a two-track function
+    let bind f = 
+        fail |> either f
 
-// compose two switches into another switch
-let (>=>) s1 s2 = 
-    s1 >> bind s2
+    // pipe a two-track value into a switch function 
+    let (>>=) x f = 
+        bind f x
 
-// convert a one-track function into a switch
-let switch f = 
-    f >> succeed
+    // compose two switches into another switch
+    let (>=>) s1 s2 = 
+        s1 >> bind s2
 
-// convert a one-track function into a two-track function
-let map f = 
-    either (f >> succeed) fail
+    // convert a one-track function into a switch
+    let switch f = 
+        f >> succeed
 
-// convert a dead-end function into a one-track function
-let tee f x = 
-    f x; x 
+    // convert a one-track function into a two-track function
+    let map f = 
+        either (f >> succeed) fail
 
-// convert a one-track function into a switch with exception handling
-let tryCatch f exnHandler x =
-    try
-        f x |> succeed
-    with
-    | ex -> exnHandler ex |> fail
+    // convert a dead-end function into a one-track function
+    let tee f x = 
+        f x; x 
 
-// convert two one-track functions into a two-track function
-let doubleMap successFunc failureFunc =
-    either (successFunc >> succeed) (failureFunc >> fail)
+    // convert a one-track function into a switch with exception handling
+    let tryCatch f exnHandler x =
+        try
+            f x |> succeed
+        with
+        | ex -> exnHandler ex |> fail
 
-// add two switches in parallel
-let plus addSuccess addFailure switch1 switch2 x = 
-    match (switch1 x),(switch2 x) with
-    | Success s1,Success s2 -> Success (addSuccess s1 s2)
-    | Failure f1,Success _  -> Failure f1
-    | Success _ ,Failure f2 -> Failure f2
-    | Failure f1,Failure f2 -> Failure (addFailure f1 f2)
+    // convert two one-track functions into a two-track function
+    let doubleMap successFunc failureFunc =
+        either (successFunc >> succeed) (failureFunc >> fail)
+
+    // add two switches in parallel
+    let plus addSuccess addFailure switch1 switch2 x = 
+        match (switch1 x),(switch2 x) with
+        | Success s1,Success s2 -> Success (addSuccess s1 s2)
+        | Failure f1,Success _  -> Failure f1
+        | Success _ ,Failure f2 -> Failure f2
+        | Failure f1,Failure f2 -> Failure (addFailure f1 f2)
 
 
-// Alias
-type CSResult<'a> = SFX.ROP.CSharp.Result<'a>
+    // Alias
+    type CSResult<'a> = SFX.ROP.CSharp.Result<'a>
 
-// Convert a C# Result<'a> to the local sum type
-let toResult (x: CSResult<'a>) =
-    if x.Error |> isNull then x.Error |> fail
-    else x.Value |> succeed
+    // Convert a C# Result<'a> to the local sum type
+    let toResult (x: CSResult<'a>) =
+        if x.Error |> isNull then x.Error |> fail
+        else x.Value |> succeed
